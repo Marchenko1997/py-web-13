6d777d9dd24d00a980d604aed68f515933658033
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-07-09 11:57:18 +0200

    updating auth py

--- contacts_api/src/routes/auth.py
+++ contacts_api/src/routes/auth.py
@@ -37,30 +37,28 @@ async def signup(body: UserModel, db: Session = Depends(get_db)):
 async def login(
     body: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
 ):
-    
+
     user = await repository_users.get_user_by_email(body.username, db)
 
- 
     if user is None:
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
-   
     if not user.confirmed:
         raise HTTPException(status_code=401, detail="Email not verified")
 
-   
     if not auth_service.verify_password(body.password, user.password):
         raise HTTPException(status_code=401, detail="Wrong password")
 
-  
     access_token = await auth_service.create_access_token(data={"sub": user.email})
     refresh_token = await auth_service.create_refresh_token(data={"sub": user.email})
 
-   
     await repository_users.update_token(user, refresh_token, db)
 
-   
-    return {"access_token": access_token, "refresh_token": refresh_token}
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
 
 
 @router.get("/refresh_token", response_model=TokenModel)
@@ -76,7 +74,11 @@ async def refresh_token(
     access_token = await auth_service.create_access_token(data={"sub": email})
     refresh_token = await auth_service.create_refresh_token(data={"sub": email})
     await repository_users.update_token(user, refresh_token, db)
-    return {"access_token": access_token, "refresh_token": refresh_token}
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
 
 
 @router.post("/logout", status_code=204)
